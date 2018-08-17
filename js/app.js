1f08fb85cea66ffa35d4f387c3c63b0e50364dd5
nadjayanna <nadjayannabr@gmail.com>
2018-08-17 11:11:07 +0200

    Game over

--- js/app.js
+++ js/app.js
@@ -38,8 +38,16 @@ function draw() {
   if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
-  if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+  if(y + dy < ballRadius) {
     dy = -dy;
+  } else if(y + dy > canvas.height-ballRadius) {
+    if(x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    }
+    else {
+      alert("GAME OVER");
+      document.location.reload();
+    }
   }
 
   x += dx;
